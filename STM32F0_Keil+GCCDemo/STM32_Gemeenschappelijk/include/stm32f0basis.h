0f8c65e288863b7dbadba76709e90086612ab900
Victoriscool <victor.v.wijhe@hotmail.com>
2016-09-14 17:27:54 +0200

    voorbeeld project toegevoegd

---
+++ STM32F0_Keil+GCCDemo/STM32_Gemeenschappelijk/include/stm32f0basis.h
@@ -0,0 +1,55 @@
+/**
+  ******************************************************************************
+  * @file    Driver voor STM32F0
+  * @author  Ewout Boks
+  * @version V1.1.1
+  * @date    15-12-2015
+  * @brief   Aangepaste driver voor de STM32F0
+  *            
+  *                 
+  * $Id: stm32f0basis.h 2039 2016-01-05 16:26:34Z ewout $ */
+	
+#ifndef STM32F0Basis_H
+#define STM32F0Basis_H
+#include <stdint.h>
+#include <stm32f0xx.h>
+
+typedef unsigned int Teller;
+
+void wachtklokInit(void);
+
+void wachtMilliSecs(const uint32_t msecs);	 
+
+class LEDDriver
+{
+	public:
+		/*! @brief initialiseer de LEDs op het discovery bord*/
+		LEDDriver();
+	
+	typedef enum
+	{
+		LED3=0,
+		LED4,
+		LEDAantalLEDs
+	} LEDid;
+
+	/*! @brief Schakel een LED aan/uit.
+		*  @param LED Id.
+		*  @param aan (true) of uit (false) */
+		void schakel(const LEDid,
+									const bool);
+		/*! @brief Schakel een LED om.
+			*  @param LED Id. */
+		void om(const LEDid);
+
+	private:
+		static const uint16_t pinSources[LEDAantalLEDs];
+};
+
+/*! @brief initialiseer de uart */
+void USARTinit(void);
+
+	 /*! @brief stuur een tekststring de uart uit */
+void USARTsend(char const * );
+	 
+#endif 
---
+++ f3demo/main.cpp
@@ -0,0 +1,84 @@
+/**
+  ******************************************************************************
+  * @file    SysTick/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    19-September-2011
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************  
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include <main.h>
+#include <leds.h>
+#include <stm32f3_discovery.h>
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+GPIO_InitTypeDef GPIO_InitStructure;
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief   Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       file (startup_stm32f3xx.s) before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, refer to
+        system_stm32f3xx.c file
+     */     
+    LEDController ledController;
+
+    ledController.tim3Demo();
+
+}
+
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
