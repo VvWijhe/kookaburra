879bc522946eeee7629df9ddfdb70349bdda4608
Victoriscool <victor.v.wijhe@hotmail.com>
2016-09-19 15:07:30 +0200

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit bcc54b225c40d3f2540d7d7d085abaa1e0b0585c.

---
+++ buildsystem/STM32F30.cmake
@@ -0,0 +1,43 @@
+## Copyright 2011-2014 Hogeschool van Arnhem en Nijmegen
+## Opleiding Embedded Systems Engineering
+## Ewout Boks
+## $Id: STM32F30.cmake 2279 2016-08-29 15:00:23Z ewout $
+ 
+message(STATUS "Bouw target voor STM32F30x met 256 KByte Flash en 40K SRAM")
+
+include(${CMAKESCRIPTDIR}/BasisSTM32.cmake)
+
+add_definitions(-DARMCORTEXM4)
+
+set(stm32f3cmsisdir ${STLIBRARIES}/CMSIS/Device/ST/STM32F30x)
+set(stm32f3perifdir ${STLIBRARIES}/STM32F30x_StdPeriph_Driver)
+set(stm32firmwareinclude  ${STLIBRARIES}/CMSIS/Include ${stm32f3cmsisdir}/Include ${stm32f3perifdir}/inc)
+set(stm32linkermap ${STLIBRARIES}/ldscripts/STM32F30x_256k_40k_flash.ld)
+
+set (CMAKE_ASM-ATT_FLAGS ${CMAKE_ASM_CM4VLAGGEN})
+set (CMAKE_C_FLAGS "${CMAKE_C_CM4FVLAGGEN}")
+set (CMAKE_CXX_FLAGS ${CMAKE_CXX_CM4FVLAGGEN})
+set (CMAKE_C_LINK_FLAGS "${CMAKE_C_CM4FVLAGGEN} -T ${stm32linkermap} -Wl,-Map,${PROJECTNAAM}.map -nostartfiles")
+#   -Xlinker --defsym=_isatty=_isatty_r
+set(CMAKE_CXX_LINK_FLAGS ${CMAKE_C_LINK_FLAGS})
+
+include_directories(${stm32firmwareinclude})
+
+add_subdirectory(${stm32f3cmsisdir} ${CMAKE_CURRENT_BINARY_DIR}/STM32CMSIS)
+add_subdirectory(${stm32f3perifdir} ${CMAKE_CURRENT_BINARY_DIR}/STM32_StdPeriph_Driver)
+
+if (USE_USBDEVICE)
+
+ message(STATUS "STM32 USB FS Device driver will be included")
+ include_directories(${STLIBRARIES}/STM32_USB-FS-Device_Driver/inc)
+ add_subdirectory(${STLIBRARIES}/STM32_USB-FS-Device_Driver ${CMAKE_CURRENT_BINARY_DIR}/STM32_USB-FS-Device_Driver)
+endif()
+
+if (USE_I2C_CPAL)
+message(STATUS "STM32F30x I2C CPAL v2 driver will be included")
+ include_directories(${STLIBRARIES}/STM32_CPALv2_Driver/inc)
+ add_subdirectory(${STLIBRARIES}/STM32_CPALv2_Driver ${CMAKE_CURRENT_BINARY_DIR}/STM32_CPALv2_Driver)
+endif()
+
+
+
---
+++ discoveryf4Demo/leds.cpp
@@ -0,0 +1,181 @@
+/**
+  ******************************************************************************
+
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************  
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include <leds.h>
+#include <stm32f4_discovery.h>
+#include <stm32f4xx_tim.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void TIM3_IRQHandler(void)
+{
+  if (TIM_GetITStatus(TIM3, TIM_IT_CC1) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM3, TIM_IT_CC1);
+
+    /* LED4 toggling with frequency = 4.57 Hz */
+    STM_EVAL_LEDToggle(LED4);
+		LEDController::capture = TIM_GetCapture1(TIM3);
+    TIM_SetCompare1(TIM3, LEDController::capture + LEDController::CCR1_Val);
+  }
+  else if (TIM_GetITStatus(TIM3, TIM_IT_CC2) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM3, TIM_IT_CC2);
+
+    /* LED3 toggling with frequency = 9.15 Hz */
+    STM_EVAL_LEDToggle(LED3);
+    LEDController::capture = TIM_GetCapture2(TIM3);
+    TIM_SetCompare2(TIM3, LEDController::capture + LEDController::CCR2_Val);
+  }
+  else if (TIM_GetITStatus(TIM3, TIM_IT_CC3) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM3, TIM_IT_CC3);
+
+    /* LED5 toggling with frequency = 18.31 Hz */
+    STM_EVAL_LEDToggle(LED5);
+    LEDController::capture = TIM_GetCapture3(TIM3);
+    TIM_SetCompare3(TIM3, LEDController::capture + LEDController::CCR3_Val);
+  }
+  else
+  {
+    TIM_ClearITPendingBit(TIM3, TIM_IT_CC4);
+
+    /* LED6 toggling with frequency = 36.62 Hz */
+    STM_EVAL_LEDToggle(LED6);
+    LEDController::capture = TIM_GetCapture4(TIM3);
+    TIM_SetCompare4(TIM3, LEDController::capture + LEDController::CCR4_Val);
+  }
+}
+#ifdef __cplusplus
+}
+#endif
+
+LEDController::LEDController() : STM32F4Controller(),i2c(I2C1)
+{
+	tim3Init();
+}
+
+uint16_t LEDController::capture=0;
+const __IO uint16_t LEDController::CCR1_Val = 54618;
+const __IO uint16_t LEDController::CCR2_Val = 27309;
+const __IO uint16_t LEDController::CCR3_Val = 13654;
+const __IO uint16_t LEDController::CCR4_Val = 6826;
+uint16_t LEDController::PrescalerValue = 0;
+
+void LEDController::tim3Init(void)
+{
+  NVIC_InitTypeDef NVIC_InitStructure;
+
+  /* TIM3 clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+  /* Enable the TIM3 global Interrupt */
+  NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+}
+
+void  LEDController::tim3Demo()
+{
+  /* -----------------------------------------------------------------------
+    TIM3 Configuration: Output Compare Timing Mode:
+    
+    In this example TIM3 input clock (TIM3CLK) is set to 2 * APB1 clock (PCLK1), 
+    since APB1 prescaler is different from 1.   
+      TIM3CLK = 2 * PCLK1  
+      PCLK1 = HCLK / 4 
+      => TIM3CLK = HCLK / 2 = SystemCoreClock /2
+          
+    To get TIM3 counter clock at 50 MHz, the prescaler is computed as follows:
+       Prescaler = (TIM3CLK / TIM3 counter clock) - 1
+       Prescaler = ((SystemCoreClock /2) /50 MHz) - 1
+                                              
+    CC1 update rate = TIM3 counter clock / CCR1_Val = 9.154 Hz
+    ==> Toggling frequency = 4.57 Hz
+    
+    C2 update rate = TIM3 counter clock / CCR2_Val = 18.31 Hz
+    ==> Toggling frequency = 9.15 Hz
+    
+    CC3 update rate = TIM3 counter clock / CCR3_Val = 36.62 Hz
+    ==> Toggling frequency = 18.31 Hz
+    
+    CC4 update rate = TIM3 counter clock / CCR4_Val = 73.25 Hz
+    ==> Toggling frequency = 36.62 Hz
+
+    Note: 
+     SystemCoreClock variable holds HCLK frequency and is defined in system_stm32f4xx.c file.
+     Each time the core clock (HCLK) changes, user had to call SystemCoreClockUpdate()
+     function to update SystemCoreClock variable value. Otherwise, any configuration
+     based on this variable will be incorrect.    
+  ----------------------------------------------------------------------- */  
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef  TIM_OCInitStructure;
+  /* Compute the prescaler value */
+  PrescalerValue = (uint16_t) ((SystemCoreClock / 2) / 500000) - 1;
+
+  /* Time base configuration */
+  TIM_TimeBaseStructure.TIM_Period = 65535;
+  TIM_TimeBaseStructure.TIM_Prescaler = 0;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+  TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+
+  /* Prescaler configuration */
+  TIM_PrescalerConfig(TIM3, PrescalerValue, TIM_PSCReloadMode_Immediate);
+
+  /* Output Compare Timing Mode configuration: Channel1 */
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_Timing;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+  TIM_OC1Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Disable);
+
+  /* Output Compare Timing Mode configuration: Channel2 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR2_Val;
+
+  TIM_OC2Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Disable);
+
+  /* Output Compare Timing Mode configuration: Channel3 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR3_Val;
+
+  TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Disable);
+
+  /* Output Compare Timing Mode configuration: Channel4 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR4_Val;
+
+  TIM_OC4Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Disable);
+   
+  /* TIM Interrupts enable */
+  TIM_ITConfig(TIM3, TIM_IT_CC1 | TIM_IT_CC2 | TIM_IT_CC3 | TIM_IT_CC4, ENABLE);
+
+  /* TIM3 enable counter */
+  TIM_Cmd(TIM3, ENABLE);
+	
+	while(1);  /* blijf hier hangen */
+}
+
+
+
