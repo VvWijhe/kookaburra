b3cb0dbca09792a647dc69675a3c6ea8570bb588
Victoriscool <victor.v.wijhe@hotmail.com>
2016-09-19 15:28:17 +0200

    Revert "???"
    This reverts commit df63663987e29431d6daa38ef15457b0f59aac0c.

---
+++ gemeenschappelijk/Third_Party/FatFs_vR0.08a/tijd.c
@@ -0,0 +1,24 @@
+#include "diskio.h"
+#include "ffconf.h"
+
+/*-----------------------------------------------------------------------*/
+/* Get current time                                                      */
+/*-----------------------------------------------------------------------*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DWORD get_fattime ()
+{
+	return	((2006UL-1980) << 25)	      // Year = 2006
+			| (2UL << 21)	      // Month = Feb
+			| (9UL << 16)	      // Day = 9
+			| (22U << 11)	      // Hour = 22
+			| (30U << 5)	      // Min = 30
+			| (0U >> 1)	      // Sec = 0
+			;
+}
+
+#ifdef __cplusplus
+}
+#endif
