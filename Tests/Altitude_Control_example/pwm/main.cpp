1b12ca87e0d4544617841c5bb60fdb430132326b
jamiecheng <jamiecheng97@gmail.com>
2016-12-05 14:20:59 +0100

    Altitude control 0.1

--- Tests/Altitude_Control_example/pwm/main.cpp
+++ Tests/Altitude_Control_example/pwm/main.cpp
@@ -1,66 +1,29 @@
-#include <stm32f0xx.h>
 #include <pwm.h>
-#include <cmath>
-#include <stm32f0_discovery.h>
 #include <pid.h>
-#include "MPU6050.h"
+#include <sched.h>
 
-#define  MAX_ANGLE 20
-#define  MIN_ANGLE -20
-#define CONVERSIONG 3.9
+#define ALT_SETPOINT 10
 
 PWM pwm;
-MPU6050 accelerometer;
+
+/// TBD: NEED BAROMETER
 
 int main(void) {
-    pwm.initServo();
-    accelerometer.init();
-
-    STM_EVAL_LEDInit(LED4);
-    STM_EVAL_LEDInit(LED3);
+    pwm.initMotor();
 
     while (1) {
-        accelGyroDataRaw_t accelGyroDataRaw;
+        PID pid(0.02, 1, 0, 0);
+        double altitudeControlValue;
 
-        if (accelerometer.testConnection()) {
-            STM_EVAL_LEDToggle(LED4);
-        } else {
-            STM_EVAL_LEDToggle(LED3);
-        }
+        altitudeControlValue = pid.calculate(barometerHeight, ALT_SETPOINT);
 
-        accelerometer.getRawAccelGyro(&accelGyroDataRaw);
+        // Set to PWM
+        altitudeControlValue *= 0.04;
 
-        double accelerationX = (accelGyroDataRaw.Ax * CONVERSIONG);
-        double accelerationY = (accelGyroDataRaw.Ay * CONVERSIONG);
-        double accelerationZ = (accelGyroDataRaw.Az * CONVERSIONG);
+        if(altitudeControlValue < 0) altitudeControlValue = 0;
+        if(altitudeControlValue > 100) altitudeControlValue = 100;
 
-        __volatile double pitch =
-                180 * atan(accelerationX / sqrt(accelerationY * accelerationY + accelerationZ * accelerationZ)) / M_PI;
-        //double roll = 180 * atan (accelerationY/sqrt(accelerationX*accelerationX + accelerationZ*accelerationZ))/M_PI;
-        //double yaw = 180 * atan (accelerationZ/sqrt(accelerationX*accelerationX + accelerationZ*accelerationZ))/M_PI;
-        //pitch += 90;
-
-        if (pitch > MAX_ANGLE || pitch < MIN_ANGLE) {
-            PID pidPitch(0.02, 15, 0, 0);
-            double pitchControlValue = 0;
-
-            // Error an control value
-            if (pitch > MAX_ANGLE) {
-                pitchControlValue = pidPitch.calculate(pitch, MAX_ANGLE);
-            } else {
-                pitchControlValue = pidPitch.calculate(pitch, MIN_ANGLE);
-            }
-            //pitchControlValue *= 0.1;
-
-            // Saturation
-            if (pitchControlValue > 500) pitchControlValue = 495;
-            else if (pitchControlValue < -500) pitchControlValue = -495;
-
-            // Set servo
-            pwm.cycle(2, (uint32_t) (pitchControlValue + 1500));
-        } else {
-            pwm.cycle(2, (uint32_t) 1500);
-        }
+        pwm.cycle(2, (uint32_t)altitudeControlValue);
     }
 }
 
