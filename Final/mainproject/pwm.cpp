7743186bf4c31b048a1a04d412777f0585177026
jamiecheng <jamiecheng97@gmail.com>
2016-12-09 18:10:50 +0100

    Final project 0.2

---
+++ Final/mainproject/pwm.cpp
@@ -0,0 +1,128 @@
+//
+// Created by Jamie on 10-11-2016.
+//
+#include <stm32f0xx_conf.h>
+#include "pwm.h"
+
+AirplaneControl::AirplaneControl() :
+        motorEnabled(false),
+        servoEnbled(false) {
+
+}
+
+void AirplaneControl::initServo() {
+    GPIO_InitTypeDef GPIO_InitStructure;
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    TIM_OCInitTypeDef TIM_OCInitStructure;
+
+    //(#) Enable TIM clock using
+    //    RCC_APBxPeriphClockCmd(RCC_APBxPeriph_TIMx, ENABLE) function.
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+    //(#) Configure the TIM pins by configuring the corresponding GPIO pins
+    //    This is LED3 on STM32F0-Discovery
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_2);
+
+    //(#) Configure the Time base unit as described in the first part of this
+    //    driver, if needed, else the Timer will run with the default
+    //    configuration:
+    //    (++) Autoreload value = 0xFFFF.
+    //    (++) Prescaler value = 0x0000.
+    //    (++) Counter mode = Up counting.
+    //    (++) Clock Division = TIM_CKD_DIV1.
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseStructure.TIM_Period = 20000 - 1;
+    TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t) ((SystemCoreClock / 1000000) - 1);
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+    //(#) Fill the TIM_OCInitStruct with the desired parameters including:
+    //    (++) The TIM Output Compare mode: TIM_OCMode.
+    //    (++) TIM Output State: TIM_OutputState.
+    //    (++) TIM Pulse value: TIM_Pulse.
+    //    (++) TIM Output Compare Polarity : TIM_OCPolarity.
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = 0;
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+    //(#) Call TIM_OCxInit(TIMx, &TIM_OCInitStruct) to configure the desired
+    //    channel with the corresponding configuration.
+    TIM_OC4Init(TIM2, &TIM_OCInitStructure);
+
+    //(#) Call the TIM_Cmd(ENABLE) function to enable the TIM counter.
+    TIM_Cmd(TIM2, ENABLE);
+
+    servoEnbled = true;
+}
+
+void AirplaneControl::initMotor() {
+    GPIO_InitTypeDef GPIO_InitStructure;
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    TIM_OCInitTypeDef TIM_OCInitStructure;
+
+    //(#) Enable TIM clock using
+    //    RCC_APBxPeriphClockCmd(RCC_APBxPeriph_TIMx, ENABLE) function.
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM14, ENABLE);
+
+    //(#) Configure the TIM pins by configuring the corresponding GPIO pins
+    //    This is LED3 on STM32F0-Discovery
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource1, GPIO_AF_0);
+
+    //(#) Configure the Time base unit as described in the first part of this
+    //    driver, if needed, else the Timer will run with the default
+    //    configuration:
+    //    (++) Autoreload value = 0xFFFF.
+    //    (++) Prescaler value = 0x0000.
+    //    (++) Counter mode = Up counting.
+    //    (++) Clock Division = TIM_CKD_DIV1.
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseStructure.TIM_Period = 100 - 1;
+    TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t) ((SystemCoreClock / 5000) - 1);
+    TIM_TimeBaseInit(TIM14, &TIM_TimeBaseStructure);
+
+    //(#) Fill the TIM_OCInitStruct with the desired parameters including:
+    //    (++) The TIM Output Compare mode: TIM_OCMode.
+    //    (++) TIM Output State: TIM_OutputState.
+    //    (++) TIM Pulse value: TIM_Pulse.
+    //    (++) TIM Output Compare Polarity : TIM_OCPolarity.
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = 0;
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+    //(#) Call TIM_OCxInit(TIMx, &TIM_OCInitStruct) to configure the desired
+    //    channel with the corresponding configuration.
+    TIM_OC1Init(TIM14, &TIM_OCInitStructure);
+
+    //(#) Call the TIM_Cmd(ENABLE) function to enable the TIM counter.
+    TIM_Cmd(TIM14, ENABLE);
+
+    motorEnabled = true;
+}
+
+// timer        = 2 voor timer 2 en 14 voor timer 14
+// duty_cicle   = van 1 t'm 100 met voor timer 2: 1 = 1ms & 100 = 2ms
+void AirplaneControl::setDutyCycle(pwmFunction_t function, uint32_t dutyCycle) {
+    if (function == PWM_SERVO_ELEVATOR && servoEnbled) {
+        TIM_SetCompare4(TIM2, dutyCycle);
+    } else if (function == PWM_MOTOR && motorEnabled) {
+        TIM_SetCompare4(TIM14, dutyCycle);
+    } else {
+        while(1){};
+    }
+
+}
+
